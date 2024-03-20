05b36ca36090d7ee7d3f8bd5beef197c810b6446
DM <127616087+DHMBRO@users.noreply.github.com>
2024-03-20 18:14:38 +0100

    Update

---
+++ Quest Room/Assets/AllScr/Ragdoll/RagdollControler.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class RagdollControler : MonoBehaviour
+{
+    //public SetRagdoll ;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

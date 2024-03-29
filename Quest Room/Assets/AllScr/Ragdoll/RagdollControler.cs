3d2e89386569241563af3819b6ee28e9501265bb
DM <127616087+DHMBRO@users.noreply.github.com>
2024-03-29 20:09:27 +0100

    Update

--- Quest Room/Assets/AllScr/Ragdoll/RagdollControler.cs
+++ Quest Room/Assets/AllScr/Ragdoll/RagdollControler.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class RagdollControler : MonoBehaviour
 {
-    //public SetRagdoll ;
+    public SetRagdoll SetRagdollDelegat;
+
 
     void Start()
     {
-        
+        List<Rigidbody> AllBones = new List<Rigidbody>(GetComponentsInChildren<Rigidbody>());
+
+        for (int i = 0;i < AllBones.Count;i++)
+        {
+            AllBones[i].gameObject.AddComponent<RagdollPerformer>();
+            AllBones[i].GetComponent<RagdollPerformer>().GetReferences(this);
+        }
+
+        SetRagdollDelegat += EnableAnimator;
+
+        SetRagdollDelegat(false);
+    }
+
+    private void EnableAnimator(bool Enable)
+    {
+        GetComponent<Animator>().enabled = !Enable;
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {   
+            SetRagdollDelegat(true); 
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            SetRagdollDelegat(false);
+        }
     }
 }
--- Quest Room/Assets/AllScr/Ragdoll/RagdollPerformer.cs
+++ Quest Room/Assets/AllScr/Ragdoll/RagdollPerformer.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RagdollPerformer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void GetReferences(RagdollControler ControlerRagdoll)
     {
-        
+        ControlerRagdoll.SetRagdollDelegat += SetRigidboby;
+        ControlerRagdoll.SetRagdollDelegat += SetColision;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetRigidboby(bool EnableRagdoll)
     {
-        
+        Rigidbody LocalRigidbody = GetComponent<Rigidbody>();
+
+        LocalRigidbody.isKinematic = !EnableRagdoll;
     }
+
+    public void SetColision(bool EnableRagdoll)
+    {
+        Collider LocalCollider = GetComponent<Collider>();
+
+        LocalCollider.enabled = EnableRagdoll;
+        LocalCollider.isTrigger = !EnableRagdoll;
+    }
+
 }
